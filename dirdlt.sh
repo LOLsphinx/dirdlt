1e1449536f56cb8bce4f0f5bef2978b1f5fa8118
Sphinx <104243528+LOLsphinx@users.noreply.github.com>
2022-04-26 11:30:10 +0800

    dirdlt.sh

--- dirdlt.sh
+++ dirdlt.sh
@@ -12,11 +12,19 @@
 
 
 ## If you Copy Then Give the credits :))
+
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+
 clear
 cd ~
 pwd
 ls
-echo -n "Where do u want me to go? look at the Directories above and navigate wherever you want to go: "
+echo -n "${BLACKBG}${CYAN}Where do u want me to go? ${RED}look at the Directories above and navigate wherever you want to go: ${RESETBG}"
 read go1
 clear
 sleep 2
@@ -24,24 +32,25 @@ sleep 2
 cd $go1
 clear
 pwd
+echo -n "${RESETBG}${WHITE}All Directory: ${BLACKBG}${RED}" 
 ls
-sleep 1
+sleep 2
 
-echo -n "Enter filename to check: "
+echo -n "${RESETBG}${CYAN}Enter filename to check: "
 read fname
 
 if [[ -f "$fname" ]]
 then
-   echo "$fname exists..."
+   echo "${RESETBG}${RED}${BLACKBG}     $fname exists..."
 else
-    echo "$fname doesn't exist.."
+    echo "${RESETBG}${BLACKBG}${RED}   $fname doesn't exist.."
     sleep 2
     echo "There's no $fname Directory!"
     sleep 3
     echo "We're heading back to your Main Directory!.."
     sleep 2
     clear
-    echo "Sorry we can't really see the $fname Directory, Terminal is going to close..."
+    echo "${RESETBG}${BLACKBG}${RED}Sorry we can't really see the $fname Directory, Terminal is going to close..."
     sleep 3
     cd
     exit
@@ -50,12 +59,12 @@ fi
 sleep 2
 clear
 ls
-echo -n "The $fname filename exists what do u want to do with it [delete], [view]: "
+echo -n "${RESETBG}${BLACKBG}${WHITE} The $fname filename exists what do u want to do with it [delete], [view]: "
 read do1
 
 if [ "$do1" = "delete" ]
 then
-    echo -n "do u really want to delete it [yes] or [no]: "
+    echo -n "${RESETBG}${GREEN}${WHITE} do u really want to delete it [yes] or [no]: "
     read do2
 fi
 
